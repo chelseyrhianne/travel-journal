b1f133b02d1863b0dc72b02aebc3010d50f33d93
chelseyrhianne <87962835+chelseyrhianne@users.noreply.github.com>
2022-04-12 18:46:21 +1200

    Added data set

--- src/data.js
+++ src/data.js
@@ -1,12 +1,62 @@
 export default [
   {
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    title: "Skógafoss",
+    location: "Iceland",
+    googleMapsUrl: "https://goo.gl/maps/z1pWAR9HdEcxgupf9",
+    startDate: "21 April, 2017",
+    endDate: "29 April, 2017",
+    description:
+      "The Skógafoss is one of the biggest waterfalls in the country, with a width of 25 metres (82 feet) and a drop of 60 m (200 ft). Due to the amount of spray the waterfall consistently produces, a single or double rainbow is normally visible on sunny days. According to legend, the first Viking settler in the area, Þrasi Þórólfsson, buried a treasure in a cave behind the waterfall.",
+    imageUrl: "https://unsplash.com/photos/wR79HXsC8VI",
+  },
+  {
+    title: "Porto Limnionas",
+    location: "Zakynthos, Greece",
+    googleMapsUrl: "https://goo.gl/maps/4vs7osMNHKu2zLyz7",
+    startDate: "13 July, 2017",
+    endDate: "25 July, 2017",
+    description:
+      "Rocky inlet known for its clear water, with cave swimming and snorkeling plus a tavern with loungers.",
+    imageUrl: "https://unsplash.com/photos/AkPXFRh8rJQ",
+  },
+  {
+    title: "Milford Sound",
+    location: "Aotearoa New Zealand",
+    googleMapsUrl: "https://goo.gl/maps/rwmD8BdaVytLHXkk8",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+      "Milford Sound is a fiord in the southwest of New Zealand’s South Island. It’s known for towering Mitre Peak, plus rainforests and waterfalls like Stirling and Bowen falls, which plummet down its sheer sides.",
+    imageUrl: "https://unsplash.com/photos/HQK_tKMpvjg",
+  },
+  {
+    title: "Akumal",
+    location: "Mexico",
+    googleMapsUrl: "https://goo.gl/maps/4Qg4i2rbqpMhrocV6",
+    startDate: "18 June, 2016",
+    endDate: "2 July, 2016",
+    description:
+      "Akumal is a coastal resort on Mexico’s Riviera Maya, on the Yucatán Peninsula. Akumal Beach has white sands, a coral reef and warm waters rich in marine life, including turtles.",
+    imageUrl: "https://unsplash.com/photos/oavzdrcUidg",
+  },
+  {
+    title: "Te Paki Sand Dunes",
+    location: "Aotearoa New Zealand",
+    googleMapsUrl: "https://goo.gl/maps/RiY56Po2PoTfsAtKA",
+    startDate: "9 April, 2018",
+    endDate: "14 April, 2018",
+    description:
+      "Te Paki Sand Dunes, also called the Giant Sand Dunes, are a collection of sand dunes located on the Northland Peninsula of New Zealand. The dunes abut the Ninety Mile Beach and are a popular spot for sandboarding.",
+    imageUrl: "https://unsplash.com/photos/x9Gjo7KhXPw",
+  },
+  {
+    title: "Kelingking Beach",
+    location: "Nusa Penida, Indonesia",
+    googleMapsUrl: "https://goo.gl/maps/nMLUhQk2eu6ZpAT16",
+    startDate: "20 March, 2018",
+    endDate: "24 March, 2018",
+    description:
+      "Beach located on Nusa Penida, an island southeast of Indonesia's island Bali. Known for its famous viewpoint from above and powerful waves, the beach can accessed via the precarious staircase leading down the cliff face.",
+    imageUrl: "https://unsplash.com/photos/73FnCOHiUng",
   },
 ];
