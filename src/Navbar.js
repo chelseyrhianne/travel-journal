2a9bebe20b970463b4564ddf3e8ce7ef8008d1a7
chelseyrhianne <87962835+chelseyrhianne@users.noreply.github.com>
2022-04-12 19:09:12 +1200

    Loaded fonts and Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="Navbar">
+      <img className="logo" src="../images/globe.png" alt="Globe logo" />
+      <p>mytraveljournal</p>
+    </nav>
+  );
+}
